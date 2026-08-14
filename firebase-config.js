b2543e2c0d0cb2d933ff1092c9b8cc32a22ca10d
green3077 <green3077@gmail.com>
2026-08-14 12:36:53 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase 콘솔(console.firebase.google.com) > 프로젝트 설정 > 내 앱 > 웹 앱의
+// firebaseConfig 객체 내용을 아래에 그대로 붙여넣으세요.
+// (아직 값을 채우지 않으면 앱이 "Firebase 설정이 필요합니다" 화면을 보여줍니다.)
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBPORHDEzQkm6WsJ4Ga6NtxfM3v-IElCyQ",
+  authDomain: "location-47773.firebaseapp.com",
+  databaseURL: "https://location-47773-default-rtdb.firebaseio.com",
+  projectId: "location-47773",
+  storageBucket: "location-47773.firebasestorage.app",
+  messagingSenderId: "1032334149162",
+  appId: "1:1032334149162:web:6acc1aa8d261ed9ed46553",
+};
